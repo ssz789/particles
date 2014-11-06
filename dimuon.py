49b7071ec2d9e49f5e886acdceed62af08554332
Sema Zahid <sema.zahid@brunel.ac.uk>
2014-11-06 14:10:32 +0000

    added notes

--- dimuon.py
+++ dimuon.py
@@ -1,10 +1,12 @@
 from ROOT import TFile, TTree
 
-f = TFile("events.root")
-t = f.Get("events")
-nEvents = t.GetEntries()
-print "Number of events = " + str(nEvents)
-nBytes = t.GetEntry(0)
-assert nBytes>0, "read zero bytes from TTree"
-nParticles = t .nPart
-print "Number of particles = " + str(nParticles)
+if __name__ == "__main__":
+
+   f = TFile("events.root")
+   t = f.Get("events")
+   nEvents = t.GetEntries()
+   print "Number of events = " + str(nEvents)
+   nBytes = t.GetEntry(0)
+   assert nBytes>0, "read zero bytes from TTree"
+   nParticles = t .nPart
+   print "Number of particles = " + str(nParticles)
