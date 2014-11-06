a1b607c401af2af726446bbfb08d194c7cdcb976
Pierre Lasorak <p.j.j.lasorak@qmul.ac.uk>
2014-11-06 15:11:49 +0000

    pairs finding

--- dimuon.py
+++ dimuon.py
@@ -2,14 +2,18 @@ from ROOT import TFile, TTree
 
 def find_pairs(particles):
     num_particles = len(particles)
+    #print(num_particles)
     pairs = []
-    for 1_first in xrange (num_particles):
-        for 1_second in xrange(1_first + 1, num_particles):
+    print(range(4,4))
+    for first in range(0, num_particles):
+        for second in range(first + 1, num_particles):
+            print("im here")
             pairs.append(None)
+    print("pair size" + str(len(pairs)))
     return pairs
 
 if __name__ == "__main__":
-
+    find_pairs([])
     f = TFile("events.root")
     t = f.Get("events")
     nEvents = t.GetEntries()
--- test_dimuon.py
+++ test_dimuon.py
@@ -6,11 +6,19 @@ def test_zero_particles():
     assert len(pairs)==0
 
 def test_one_particle():
-    partciles = [None]
+    particles = [None]
     pairs = find_pairs(particles)
     assert len(pairs)==0
 
 def test_two_particle():
-    partciles = [None,None]
+    particles = [None,None]
     pairs = find_pairs(particles)
     assert len(pairs)==1
+
+
+def test_5_particle():
+    particles = [None,None,None,None,None]
+    pairs = find_pairs(particles)
+    assert len(pairs)==10
+
+
