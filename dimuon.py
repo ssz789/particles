17a941f950dd6cbc3e405fad236e04b3670266e1
Pierre Lasorak <p.j.j.lasorak@qmul.ac.uk>
2014-11-06 16:27:00 +0000

    4momentum calculation

--- dimuon.py
+++ dimuon.py
@@ -1,15 +1,46 @@
-from ROOT import TFile, TTree
+from ROOT import TFile
+from ROOT import TTree
+
+class particle:
+    def __init__(self, charge, qpt, E):
+        self.charge = charge
+        self.qpt = qpt
+        self.E = E
+    def charge(self):
+        return self.charge
+
+class p4momentum:
+    def __init__(self, e, px, py, pz):
+        self.e = e
+        self.px = px
+        self.py = py
+        self.pz = pz
+
+    def mass(self):
+        e = self.e
+        px = self.px
+        py = self.py
+        pz = self.pz
+        return (e*e - px*px - py*py - pz*pz)**(0.5)
+ 
+    def invmass(self, other):
+        e = self.e   + other.e
+        px = self.px + other.px
+        py = self.py + other.py
+        pz = self.pz + other.pz
+        return (e*e - px*px - py*py - pz*pz)**(0.5)
 
 def find_pairs(particles):
     num_particles = len(particles)
     #print(num_particles)
     pairs = []
-    print(range(4,4))
+    #print(range(4,4))
     for first in range(0, num_particles):
         for second in range(first + 1, num_particles):
-            print("im here")
-            pairs.append(None)
-    print("pair size" + str(len(pairs)))
+            #print("im here")
+            if particles[first].charge() == -particles[second].charge():
+                pairs.append([first, second])
+    #print("pair size" + str(len(pairs)))
     return pairs
 
 if __name__ == "__main__":
--- test_dimuon.py
+++ test_dimuon.py
@@ -1,4 +1,26 @@
+from dimuon import particle
+from dimuon import p4momentum
 from dimuon import find_pairs
+from nose.tools import assert_equal
+
+def test_zero_p4Momentum():
+    t = p4momentum(0,0,0,0)
+    assert t.mass() == 0
+
+def test_stationary_p4Momentum():
+    t = p4momentum(1,0,0,0)
+    assert t.mass() == 1
+
+def test_stationarytimesnothing_p4Momentum():
+    t1 = p4momentum(9,1,1,1)
+    t2 = p4momentum(0,0,0,0)
+    assert_equal(t1.invmass(t2),2)
+
+def test_stationarytimesphoton_p4Momentum():
+    t1 = p4momentum(1,0,0,0)
+    t2 = p4momentum(1,0,0,0)
+    assert_equal(t1.invmass(t2),2)
+
 
 def test_zero_particles():
     particles = []
@@ -9,16 +31,28 @@ def test_one_particle():
     particles = [None]
     pairs = find_pairs(particles)
     assert len(pairs)==0
-
+"""
 def test_two_particle():
     particles = [None,None]
     pairs = find_pairs(particles)
     assert len(pairs)==1
 
-
 def test_5_particle():
     particles = [None,None,None,None,None]
     pairs = find_pairs(particles)
-    assert len(pairs)==10
+    assert len(pairs)==0
 
+def test_3_real_particles():
+    particle_list = []
+    
+    p1 = particle(+1,20,10)
+    p2 = particle(-1,2,17)
+    p3 = particle(+1,3,12)
+    particle_list.append(p1)
+    particle_list.append(p2)
+    particle_list.append(p3)
 
+    pair_list = find_pairs(particle_list)
+    assert pair_list == ([0,1], [1,2])
+
+"""
