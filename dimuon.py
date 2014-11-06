4d76e45007f86ce02271dfd6b37593f7f723f196
Sema Zahid <sema.zahid@brunel.ac.uk>
2014-11-06 12:33:52 +0000

    test assert

--- dimuon.py
+++ dimuon.py
@@ -2,3 +2,7 @@ from ROOT import TFile, TTree
 
 f = TFile("events.root")
 t = f.Get("events")
+nEvents = t.GetEntries()
+print "Number of events = " + str(nEvents)
+nBytes = t.GetEntry(0)
+assert nBytes>0, "read zero bytes from TTree"
