0a1b8364790a68be1c5398e75b6c7f9a0f29d75b
Sema Zahid <sema.zahid@brunel.ac.uk>
2014-11-06 12:46:56 +0000

    number of partcls of one event

--- dimuon.py
+++ dimuon.py
@@ -6,3 +6,5 @@ nEvents = t.GetEntries()
 print "Number of events = " + str(nEvents)
 nBytes = t.GetEntry(0)
 assert nBytes>0, "read zero bytes from TTree"
+nParticles = t .nPart
+print "Number of particles = " + str(nParticles)
