3532a4723ac06e2b9f94f5a88c94cc0210325868
Sema Zahid <sema.zahid@brunel.ac.uk>
2014-11-06 14:42:34 +0000

    new

--- test_dimuon.py
+++ test_dimuon.py
@@ -1,5 +1,16 @@
 from dimuon import find_pairs
 
-particles = []
-pairs = find_pairs(particles)
-assert len(pairs)==0 
+def test_zero_particles():
+    particles = []
+    pairs = find_pairs(particles)
+    assert len(pairs)==0
+
+def test_one_particle():
+    partciles = [None]
+    pairs = find_pairs(particles)
+    assert len(pairs)==0
+
+def test_two_particle():
+    partciles = [None,None]
+    pairs = find_pairs(particles)
+    assert len(pairs)==1
